0728a0a82fce7ab9e8a08379f57e994b10fbe9ad
Jerome Martinez <jerome.e.martinez@gmail.com>
2016-07-21 22:04:43 -0500

    starting

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,15 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var minifycss = require('gulp-minify-css');
 var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('build', ['styles', 'scripts', 'images']);
+
+gulp.task('images', function() {
+    return gulp.src('src/images/**/*')
+        .pipe(gulp.dest('dist/images/'))
+        .pipe($.size());
+});
 
 gulp.task('clean', function(cb) {
     del.sync(['dist']);
@@ -21,13 +30,27 @@ gulp.task('browser-sync', function() {
   });
 });
 
+gulp.task('watch', ['build'], function() {
+    browserSync({
+        proxy: "http://simply.api.dev/",
+        notify: false,
+        logPrefix: 'BS',
+    });
+    gulp.watch(['app/simply/**/*',
+                'app/admin/**/*',
+                'app/shared/**/*',
+                'app/property-owner/**/*',
+                'app/assets/styles/**/*',
+                'app/views/**/*'], ['slim-dep-fast', reload]);
+});
+
 gulp.task('bs-reload', function () {
   browserSync.reload();
 });
 
 gulp.task('styles', ['sass', 'css']);
 gulp.task('css', function(){
-    gulp.src(['src/styles/**/*.css'], { base: 'src/styles' })
+    gulp.src(['src/styles/**/*.css'])
     .pipe(gulp.dest('dist/styles'));
 });
 gulp.task('sass', function() {
@@ -38,22 +61,6 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('dist/styles'))
         .pipe($.size());
 });
-
-// gulp.task('styles', function(){
-//   gulp.src(['src/styles/**/*.scss'])
-//     .pipe(plumber({
-//       errorHandler: function (error) {
-//         console.log(error.message);
-//         this.emit('end');
-//     }}))
-//     .pipe(sass())
-//     .pipe(autoprefixer('last 2 versions'))
-//     .pipe(gulp.dest('dist/styles/'))
-//     .pipe(rename({suffix: '.min'}))
-//     .pipe(minifycss())
-//     .pipe(gulp.dest('dist/styles/'))
-//     .pipe(browserSync.reload({stream:true}))
-// });
 
 gulp.task('scripts', function(){
   return gulp.src('src/scripts/**/*.js')
@@ -71,6 +78,7 @@ gulp.task('scripts', function(){
 });
 
 gulp.task('default', ['browser-sync'], function(){
+  gulp.watch("src/images/**/*", ['images']);
   gulp.watch("src/styles/**/*.scss", ['styles']);
   gulp.watch("src/scripts/**/*.js", ['scripts']);
   gulp.watch("*.html", ['bs-reload']);
